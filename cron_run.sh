8b42a3adf83e7b61d95d3f877c5c639200a05373
michael1788 <mrgmoret@gmail.com>
2025-02-04 14:48:55 -0500

    running yesterday and today

--- cron_run.sh
+++ cron_run.sh
@@ -3,7 +3,10 @@
 source ~/.zshrc
 conda activate krap
 
+YESTERDAY=$(date -v-1d +"%d-%m-%y")
 TODAY=$(date +"%d-%m-%y")
 
 # Run the Python script with today's date
 python /Users/michaelmoret/tinyverse/krap/plot_analysis.py --date $TODAY
+
+python /Users/michaelmoret/tinyverse/krap/plot_analysis.py --date $YESTERDAY
